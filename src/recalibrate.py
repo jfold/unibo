18c8a2995adc7fce5cc73c55b049b97ab0570a46
jfold <jonathan.foldager@gmail.com>
2021-12-16 10:37:52 +0100

    Update recalibrator

--- src/recalibrate.py
+++ src/recalibrate.py
@@ -36,7 +36,6 @@ class Recalibrator(object):
         cv = KFold(n_splits=n_splits) if K_fold else LeaveOneOut()
         dataset_ = copy.deepcopy(dataset)
         F_t = []
-        P_hat = []
         for train_index, test_index in cv.split(X):
             X_train, X_test = X[[train_index], :], X[[test_index], :]
             y_train, y_test = y[[train_index], :], y[[test_index], :]
@@ -45,10 +44,9 @@ class Recalibrator(object):
             optimizer.fit_surrogate(dataset_)
             f_t = optimizer.surrogate_object.cdf(X_test, y_test).squeeze()
             F_t.extend(f_t)
-            P_hat.extend(calibration.calculate_p_hat(f_t))
 
+        P_hat = np.array([calibration.calculate_p_hat(F_t, f_t) for f_t in F_t])
         F_t = np.array(F_t).squeeze()
         P_hat = np.array(P_hat).squeeze()
-        print(F_t, P_hat)
         R = self.train_recalibrator_model(F_t, P_hat)
         return R
