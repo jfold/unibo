1daf9754cc038657f643fa5ac68e40d38ee72ab2
jfold <jonathan.foldager@gmail.com>
2021-12-21 16:20:22 +0100

    Update main test

--- tests/main_test.py
+++ tests/main_test.py
@@ -9,7 +9,7 @@ surrogates = [
     "RF",
 ]
 dims = [1]
-seeds = list(range(5))
+seeds = list(range(3))
 data = {
     "Benchmark": {
         "location": "datasets.benchmarks.benchmark",
@@ -22,7 +22,7 @@ data = {
 }
 kwargs = {
     "savepth": os.getcwd() + "/results/tests/",
-    "n_evals": 10,
+    "n_evals": 3,
     "plot_it": True,
     "vanilla": True,
 }
@@ -40,6 +40,7 @@ class MainTest(unittest.TestCase):
                                 {
                                     "seed": seed,
                                     "d": d,
+                                    "bo": True,
                                     "surrogate": surrogate,
                                     "data_class": data_name,
                                     "data_location": info["location"],
@@ -62,6 +63,7 @@ class MainTest(unittest.TestCase):
                                 {
                                     "seed": seed,
                                     "d": d,
+                                    "bo": False,
                                     "surrogate": surrogate,
                                     "data_class": data_name,
                                     "data_location": info["location"],
@@ -70,7 +72,7 @@ class MainTest(unittest.TestCase):
                             )
                             parameters = Parameters(kwargs_, mkdir=True)
                             experiment = Experiment(parameters)
-                            experiment.run_calibration_demo()
+                            experiment.run()
                             assert experiment.dataset.data.X.shape == (
                                 parameters.n_evals + parameters.n_initial,
                                 parameters.d,
