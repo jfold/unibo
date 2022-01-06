e63de086aa19d940fac2d1c4908be5908c8b3eb7
jfold <jonathan.foldager@gmail.com>
2022-01-06 13:46:50 +0100

    update random search

--- acquisitions/random_search.py
+++ acquisitions/random_search.py
@@ -4,4 +4,4 @@ from imports.ml import *
 
 class RandomSearch(object):
     def __call__(self, X: torch.Tensor) -> torch.Tensor:
-        return torch.rand(X.size())
+        return torch.rand(X.size()[0])
