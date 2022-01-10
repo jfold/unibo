9977dd38ce7a9a09d82fa1afe718ceae87ed0505
jfold <jonathan.foldager@gmail.com>
2022-01-10 14:36:51 +0100

    Include MI in imports

--- imports/ml.py
+++ imports/ml.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from scipy.stats import norm, uniform, kstest, entropy
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import mutual_info_regression
 import torch
 import botorch
 from botorch.models.model import Model
