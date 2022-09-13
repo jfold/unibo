893879a18a8cc11cd3ced9fdd9bdf369c1bac2fd
jfold <jonathan.foldager@gmail.com>
2022-09-13 12:34:44 +0200

    remove files

--- demo/__init__.py
+++
@@ -1,6 +0,0 @@
-from imports.ml import *
-from imports.general import *
-from src.metrics import Metrics
-from src.parameters import Parameters
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
