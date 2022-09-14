b63e5d1229509d5fc0856ad61f0d3b60eebfdc73
jfold <jonathan.foldager@gmail.com>
2022-09-14 10:55:02 +0200

    update bnn and gp

--- surrogates/bayesian_neural_network.py
+++ surrogates/bayesian_neural_network.py
@@ -16,18 +16,15 @@ class BayesianNeuralNetwork(BatchedMultiOutputGPyTorchModel):
         self.change_std = parameters.change_std
         self.std_change = parameters.std_change
         self.model = nn.Sequential(
-            # bnn.BayesLinear(
-            #     prior_mu=0.0,
-            #     prior_sigma=1.0,  # / self.d,
-            #     in_features=self.d,
-            #     out_features=50,
-            # ),
-            # nn.ReLU(),
             bnn.BayesLinear(
                 prior_mu=0.0,
-                prior_sigma=1.0,
+                prior_sigma=self.d ** (-2),
                 in_features=self.d,
-                out_features=10,  # / 50,
+                out_features=10,
+            ),
+            nn.ReLU(),
+            bnn.BayesLinear(
+                prior_mu=0.0, prior_sigma=10 ** (-2), in_features=10, out_features=10,
             ),
             nn.ReLU(),
             bnn.BayesLinear(
--- surrogates/gaussian_process.py
+++ surrogates/gaussian_process.py
@@ -28,6 +28,10 @@ class GaussianProcess(object):
         self.change_std = parameters.change_std
         self.std_change = parameters.std_change
         self.opt_hyp_pars = opt_hyp_pars
+        self.kernel = ScaleKernel(
+            RBFKernel(lengthscale_prior=LogNormalPrior(0, 1)),
+            outputscale_prior=NormalPrior(1.0, 2.0),
+        )
         self.fit(parameters, dataset)
 
     def forward(self, x: Tensor) -> MultivariateNormal:
@@ -36,21 +40,46 @@ class GaussianProcess(object):
         covar_x = torch.tensor(np.diag(covar_x.squeeze()))
         return MultivariateNormal(mean_x, covar_x)
 
+    def recalibrate(self, stabilizer: float = 1e-8):
+        assert self.X_train_ is not None and self.y_train_ is not None
+
+        F_t = []
+        y_t = self.y_train_.copy()
+        for i, x_test in enumerate(self.X_train_):
+            X = np.delete(self.X_train_.copy(), i)
+            y = np.delete(self.y_train_.copy(), i)
+            model = botorch.models.SingleTaskGP(
+                torch.tensor(X).double(),
+                torch.tensor(y).double(),
+                covar_module=self.kernel,
+            )
+            X_test_torch = torch.from_numpy(x_test)
+            posterior = model.posterior(X_test_torch.double(), observation_noise=True)
+            F_t.append(
+                norm.cdf(
+                    x_test,
+                    loc=posterior.mean.cpu().detach().numpy().squeeze(),
+                    scale=np.sqrt(posterior.variance.cpu().detach().numpy())
+                    + stabilizer,
+                )
+            )
+
     def fit(self, parameters, dataset):
+        self.X_train_ = dataset.data.X_train
+        self.y_train_ = dataset.data.y_train
+
         torch.manual_seed(parameters.seed)
-        # getattr(sys.modules[__name__], self.gp_kernel())
-        kernel = ScaleKernel(
-            RBFKernel(lengthscale_prior=LogNormalPrior(0, 1)),
-            outputscale_prior=NormalPrior(1.0, 2.0),
-        )
+
         self.model = botorch.models.SingleTaskGP(
             torch.tensor(dataset.data.X_train).double(),
             torch.tensor(dataset.data.y_train).double(),
-            covar_module=kernel,
+            covar_module=self.kernel,
         )
+
         self.likelihood = ExactMarginalLogLikelihood(self.model.likelihood, self.model)
         if self.opt_hyp_pars:
             botorch.fit.fit_gpytorch_model(self.likelihood)
+
         # surrogate_model.model.covar_module.base_kernel.lengthscale.detach().numpy().squeeze()
         # surrogate_model.model.covar_module.outputscale.detach().numpy().squeeze()
         # surrogate_model.model.likelihood.noise.detach().numpy().squeeze()
