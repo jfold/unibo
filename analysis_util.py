358c9f0111b13a3e3c503d96272359f00ff5a189
MJordahn <mikkjo@dtu.dk>
2023-02-13 20:12:06 +0100

    Pushing for other device

--- analysis_util.py
+++ analysis_util.py
@@ -18,7 +18,7 @@ def min_value_in_column(column):
     return [highlight if v == min_in_column else default for v in column]
 
 def parse_synthetic_results(problem_map, save_dict=False):
-    exp_dict = {'surrogate': [], 'acquisition': [], 'seed':[], 'problem_name':[], 'problem_idx':[], 'dim':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[]}
+    exp_dict = {'surrogate': [], 'acquisition': [], 'seed':[], 'problem_name':[], 'problem_idx':[], 'dim':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[], 'path':[]}
     main_directory="./results_synth_data/"
     for foldername in os.listdir(main_directory):
         folder = os.path.join(main_directory, foldername)
@@ -30,13 +30,13 @@ def parse_synthetic_results(problem_map, save_dict=False):
                 elif filename.find("metrics") != -1:
                     json_file = open(os.path.join(folder, filename))
                     metrics = json.load(json_file)
-            if params['bo'] == True:
+            if params['bo'] == True and params['recalibrate']==False:
                 exp_dict['surrogate'].append(params['surrogate'])
                 exp_dict['acquisition'].append(params['acquisition'])
                 exp_dict['data'].append(params['data_name'])
                 exp_dict['problem_idx'].append(params['problem_idx'])
                 exp_dict['dim'].append(params['d'])
-                exp_dict['problem_name'].append(problem_map[params[d]][params['problem_idx']])
+                exp_dict['problem_name'].append(problem_map[str(params['d'])][params['problem_idx']])
                 exp_dict['dist_nearest_train'].append(np.array(metrics['next_sample_train_distance']))
                 exp_dict['inst_regret_test'].append(np.array(metrics['y_regret_test']))
                 exp_dict['inst_regret_pool'].append(np.array(metrics['y_regret_pool']))
@@ -47,6 +47,7 @@ def parse_synthetic_results(problem_map, save_dict=False):
                 exp_dict['sharpness'].append(np.array(metrics['mean_sharpness']))
                 exp_dict['calibration_mse'].append(np.array(metrics['y_calibration_mse']))
                 exp_dict['seed'].append(params['seed'])
+                exp_dict['path'].append(folder)
     if save_dict:
         with open('parsed_results/synth_results.obj', 'wb') as save_file:
             pickle.dump(exp_dict, save_file)
@@ -119,9 +120,10 @@ def BO_performance_table1_synth_results(df):
     table_with_highlight = df.style.apply(min_value_in_column, subset=['inst_regret_pool', "tot_regret_pool", 'calibration_mse'], axis=0)
     return table_with_highlight, latex_df
 
-def parse_real_results(save_dict=False):
-    exp_dict = {'surrogate': [], 'acquisition': [], 'seed':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[]}
-    main_directory="./results_real_data/"
+def parse_real_results(save_dict=False, main_directory=None, recal=False):
+    exp_dict = {'surrogate': [], 'acquisition': [], 'seed':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'std_change':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[], 'elpd':[]}
+    if main_directory is None:
+        main_directory="./results_real_data/"
     subdirectories = ['results_FashionMNIST/', "results_FashionMNIST_CNN/", 'results_mnist/', 'results_MNIST_CNN/', 'results_News/', 'results_SVM/']
     for subdirectory in subdirectories:
         full_path = os.path.join(main_directory, subdirectory)
@@ -135,10 +137,11 @@ def parse_real_results(save_dict=False):
                     elif filename.find("metrics") != -1:
                         json_file = open(os.path.join(folder, filename))
                         metrics = json.load(json_file)
-                if params['bo'] == True:
+                if params['bo'] == True and params['recalibrate'] == recal:
                     exp_dict['surrogate'].append(params['surrogate'])
                     exp_dict['acquisition'].append(params['acquisition'])
                     exp_dict['data'].append(params['data_name'])
+                    exp_dict['std_change'].append(params['std_change'])
                     exp_dict['dist_nearest_train'].append(metrics['next_sample_train_distance'])
                     exp_dict['inst_regret_test'].append(metrics['y_regret_test'])
                     exp_dict['inst_regret_pool'].append(metrics['y_regret_pool'])
@@ -149,19 +152,25 @@ def parse_real_results(save_dict=False):
                     exp_dict['sharpness'].append(metrics['mean_sharpness'])
                     exp_dict['calibration_mse'].append(metrics['y_calibration_mse'])
                     exp_dict['seed'].append(params['seed'])
+                    exp_dict['elpd'].append(metrics['elpd'])
     df = pd.DataFrame.from_dict(exp_dict)
     if save_dict:
-        with open('parsed_results/real_results.obj', 'wb') as save_file:
-            pickle.dump(exp_dict, save_file)
+        if recal:
+            with open('parsed_results/real_results_recal.obj', 'wb') as save_file:
+                pickle.dump(exp_dict, save_file)
+        else:
+            with open('parsed_results/real_results.obj', 'wb') as save_file:
+                pickle.dump(exp_dict, save_file)   
     return df
 
 def aggregate_real_results(df):
-    processed_results = {'surrogate': [], 'acquisition': [], 'seed':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[]}
+    processed_results = {'surrogate': [], 'acquisition': [], 'seed':[], 'data':[], 'dist_nearest_train':[], 'inst_regret_test':[], 'inst_regret_pool':[], 'tot_regret_test':[], 'tot_regret_pool':[], 'std_change':[], 'calibration_mse':[], 'sharpness':[], 'x_opt_dist_test':[], 'x_opt_dist_pool':[], 'elpd':[]}
     for index, row in df.iterrows():
         processed_results['surrogate'].append(row['surrogate'])
         processed_results['acquisition'].append(row['acquisition'])
         processed_results['data'].append(row['data'])
         processed_results['seed'].append(row['seed'])
+        processed_results['std_change'].append(row['std_change'])
         processed_results['dist_nearest_train'].append(np.array(row['dist_nearest_train']).astype(float).mean()) #Mean across one BO run.
         processed_results['inst_regret_pool'].append(np.array(row['inst_regret_pool'])[-1]) #Last iter.
         processed_results['inst_regret_test'].append(np.array(row['inst_regret_test'])[-1]) #Last iter.
@@ -171,6 +180,7 @@ def aggregate_real_results(df):
         processed_results['sharpness'].append(np.array(row['sharpness']).mean()) #Mean sharpness over run.
         processed_results['x_opt_dist_test'].append(np.array(row['x_opt_dist_test'])[-1])
         processed_results['x_opt_dist_pool'].append(np.array(row['x_opt_dist_pool'])[-1])
+        processed_results['elpd'].append(row['elpd'][-1]) #Last iteration ELPD.
     df = pd.DataFrame.from_dict(processed_results)
     return df
 
@@ -207,6 +217,7 @@ def BO_performance_table1_real_results(df):
         aggregated_processed_results['sharpness_ste'].append(np.std(selection['sharpness'])/np.sqrt(len(selection['surrogate'])))
     df = pd.DataFrame.from_dict(aggregated_processed_results)
     df = df.round(5)
+    df = df.sort_values(by=['acquisition', 'surrogate'])
     latex_df = pd.DataFrame(columns=['Surrogate', 'Acquisition', 'Inst. Regret', 'Total Regret', 'Calibration Error', 'Sharpness'])
     latex_df['Surrogate'] = df['surrogate']
     latex_df['Acquisition'] = df['acquisition']
@@ -215,4 +226,4 @@ def BO_performance_table1_real_results(df):
     latex_df['Calibration Error'] = df['calibration_mse'].astype(str) + " \pm " + df['calibration_mse_ste'].astype(str)
     latex_df['Sharpness'] = df['sharpness'].astype(str) + " \pm " + df['sharpness_ste'].astype(str)
     table_with_highlight = df.style.apply(min_value_in_column, subset=['inst_regret_pool', "tot_regret_pool", 'calibration_mse'], axis=0)
-    return table_with_highlight, latex_dfs
+    return table_with_highlight, latex_df
